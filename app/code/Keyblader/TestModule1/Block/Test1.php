03a0a911d29ed4d1a0eeeff602311286f620c1f1
I4uGATbIi <i4ugatbii@gmail.com>
2019-02-02 02:34:59 +0200

    modA+modB homework + theme?

---
+++ app/code/Keyblader/TestModule1/Block/Test1.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Keyblader\TestModule1\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class Test1 extends Template
+{
+    /**
+     * @return string
+     */
+    public function getText()
+    {
+        return "Test1 test1 test1 test1 test1 test1 test1 test1 test1!";
+    }
+}
---
+++ app/code/Keyblader/TestModule2/Block/Test2.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Keyblader\TestModule2\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class Test2 extends Template
+{
+    /**
+     * @return string
+     */
+    public function getText()
+    {
+        return "Test2 Test2 Test2 Test2 Test2 Test2 Test2 Test2!";
+    }
+}
